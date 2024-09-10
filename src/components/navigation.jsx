06a404a227d732bb7d0aa8adfe5a9550acc9e5a5
eMTy Codes <reymarktimkang24@gmail.com>
2024-09-10 09:16:11 +0800

    initial file upload

---
+++ src/components/navigation.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const navigation = () => {
+  return (
+    <section>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Home</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default navigation;
